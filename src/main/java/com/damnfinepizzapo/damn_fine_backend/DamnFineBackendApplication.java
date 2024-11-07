7c0012b4703576dd4380edf5a0a213dc1ebdcd8d
bparker416 <90215187+bparker416@users.noreply.github.com>
2024-11-07 13:17:25 -0800

    Initial project creation

---
+++ src/main/java/com/damnfinepizzapo/damn_fine_backend/DamnFineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.damnfinepizzapo.damn_fine_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamnFineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamnFineBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damnfinepizzapo/damn_fine_backend/DamnFineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damnfinepizzapo.damn_fine_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamnFineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
