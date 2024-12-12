4c9c0302819fb420675d39cf0834859659ab00fb
bparker416 <90215187+bparker416@users.noreply.github.com>
2024-12-12 10:22:04 -0800

    Create authentication module

---
+++ src/main/java/com/damnfinepizzapo/damn_fine_backend/authentication/HomeController.java
@@ -0,0 +1,23 @@
+package com.damnfinepizzapo.damn_fine_backend.authentication;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "Home page.";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "Login page.";
+    }
+
+}
