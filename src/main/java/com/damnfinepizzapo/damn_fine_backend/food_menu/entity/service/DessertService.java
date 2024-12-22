f68cf5734b666f9a03365650b3e683f8f9855f8e
bparker416 <90215187+bparker416@users.noreply.github.com>
2024-12-22 14:28:54 -0800

    Update services with controller logic, again

--- src/main/java/com/damnfinepizzapo/damn_fine_backend/food_menu/entity/service/DessertService.java
+++ src/main/java/com/damnfinepizzapo/damn_fine_backend/food_menu/entity/service/DessertService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DessertService {
@@ -16,5 +17,32 @@ public class DessertService {
     public DessertService(DessertRepository dessertRepository) { this.dessertRepository = dessertRepository; }
 
     public List<Dessert> getAllDesserts() { return dessertRepository.findAllActive(); }
+
+    public Dessert createDessert(Dessert dessert) { return dessertRepository.save(dessert); }
+
+    public Optional<Dessert> getDessertById(int id) { return dessertRepository.findById(id); }
+
+    public Dessert updateDessert(int id, Dessert dessert) {
+        return dessertRepository.findById(id)
+                .map(item -> {
+                    item.setDessert_name(dessert.getDessert_name());
+                    item.setDessert_price(dessert.getDessert_price());
+                    item.setDessert_description(dessert.getDessert_description());
+                    item.setAdditional_text(dessert.getAdditional_text());
+                    item.setDessert_active(dessert.getDessert_active());
+                    return dessertRepository.save(item);
+                })
+                .orElseThrow(() -> new RuntimeException("Item not found."));
+    }
+
+    // Toggle dessert_active
+    public Dessert toggleDessertActive(int id) {
+        return dessertRepository.findById(id)
+                .map(item -> {
+                    item.setDessert_active(!item.getDessert_active());
+                    return dessertRepository.save(item);
+                })
+                .orElseThrow(() -> new RuntimeException("Item not found."));
+    }
 
 }
--- src/main/java/com/damnfinepizzapo/damn_fine_backend/food_menu/entity/service/MeatService.java
+++ src/main/java/com/damnfinepizzapo/damn_fine_backend/food_menu/entity/service/MeatService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MeatService {
@@ -21,5 +22,33 @@ public class MeatService {
 
     public List<Meat> findByPriceIsFour() {
         return meatRepository.findByPriceIsFour();
+    }
+
+    public Meat createMeat(Meat meat) { return meatRepository.save(meat); }
+
+    public Optional<Meat> getMeatById(int id) { return meatRepository.findById(id); }
+
+    public Meat updateMeat(int id, Meat meat) {
+        return meatRepository.findById(id)
+                .map(item -> {
+                    item.setMeat_name(meat.getMeat_name());
+                    item.setMeat_price(meat.getMeat_price());
+                    item.setMeat_description(meat.getMeat_description());
+                    item.setMeat_active(meat.isMeat_active());
+                    return meatRepository.save(item);
+                })
+                .orElseThrow(() -> new RuntimeException("Item not found."));
+    }
+
+    public void deleteMeat(int id) { meatRepository.deleteById(id); }
+
+    // Toggle meat_active
+    public Meat toggleMeatActive(int id) {
+        return meatRepository.findById(id)
+                .map(item -> {
+                    item.setMeat_active(!item.isMeat_active());
+                    return meatRepository.save(item);
+                })
+                .orElseThrow(() -> new RuntimeException("Item not found."));
     }
 }
